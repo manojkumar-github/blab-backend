228ea09c7cc05c12edf55709665ad70714fa348f
MJ <manoj.kumar31028@gmail.com>
2024-04-07 00:21:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
-import google.generativeai as genai
-api_key = 'AIzaSyDKeQ_CJEQdsiWSAja0C_X7veyARfITgxY'
-genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-pro')
-print("Created model!!")
-@app.route('/')
-def hello_world():
-    response = model.generate_content("Who will win Andhra Pradesh elections")
-    return response.text
+#import google.generativeai as genai
+#api_key = 'AIzaSyDKeQ_CJEQdsiWSAja0C_X7veyARfITgxY'
+#genai.configure(api_key=api_key)
+#model = genai.GenerativeModel('gemini-pro')
+#print("Created model!!")
+@app.route('/<prompt>')
+def bot_prompt():
+    #response = model.generate_content("Who will win Andhra Pradesh elections")
+    return f'Echo {prompt}'
