6f87cfd3d5403ab18a5e0b5a7a97f30650e9abac
MJ <manoj.kumar31028@gmail.com>
2024-04-06 23:53:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import google.generativeai as genai
 api_key = 'AIzaSyDKeQ_CJEQdsiWSAja0C_X7veyARfITgxY'
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-pro')
-
+print("Created model!!")
 @app.route('/')
 def hello_world():
+    print("Making response")
     response = model.generate_content("Who will win Andhra Pradesh elections")
+    print("Obtained response")
     return response.text
