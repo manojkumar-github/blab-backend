0214ebda911c5c1596a38793a74e4af630a8f6e4
MJ <manoj.kumar31028@gmail.com>
2024-04-06 23:48:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+import google.generativeai as genai
+api_key = 'AIzaSyDKeQ_CJEQdsiWSAja0C_X7veyARfITgxY'
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-pro')
 
 @app.route('/')
 def hello_world():
-    return 'Putchakalaya'
+    response = model.generate_content("Who will win Andhra Pradesh elections")
+    return response.text
