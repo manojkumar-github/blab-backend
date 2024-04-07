c037b1939db563b73f1b737057c68af9ebc8afa1
MJ <manoj.kumar31028@gmail.com>
2024-04-07 10:26:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,35 @@ app = Flask(__name__)
 #model = genai.GenerativeModel('gemini-pro')
 #print("Created model!!")
 
+
+from langchain.llms import HuggingFaceHub
+import os
+from getpass import getpass
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = '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'
+
+
+llm = HuggingFaceHub(
+    repo_id="huggingfaceh4/zephyr-7b-alpha", 
+    model_kwargs={"temperature": 0.5, "max_length": 64,"max_new_tokens":512}
+)
+
+query = "What is capital of India and UAE?"
+
+prompt = f"""
+ <|system|>
+You are an AI assistant that follows instruction extremely well.
+Please be truthful and give direct answers
+</s>
+ <|user|>
+ {query}
+ </s>
+ <|assistant|>
+"""
+
+response = llm.predict(prompt)
+print(response)
+
 @app.route('/', methods=['POST'])
 def bot_prompt():
     prompt = request.json.get('message')
